8df820abf01c472cc44ed017071230c33f6604bc
raikay <raikay@163.com>
2020-01-13 18:38:04 +0800

    automapper

---
+++ DemoList/AutoMapperDemo/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapperDemo
+{
+    public class MappingProfile : //Profile
+    {
+        public MappingProfile()
+        {
+            //添加需要映射对象
+            //CreateMap<User, UserDto>();
+            //CreateMap<UserDto, User>();
+        }
+    }
+}
