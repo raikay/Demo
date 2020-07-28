ba92d18dd48a0dc3c50be6539a0bd2557d25a2f5
Raikay <raikay@163.com>
2020-07-28 13:14:54 +0800

    save

--- 36-HttpClientFactoryDemo/HttpClientFactoryDemo/Controllers/OrderController.cs
+++ 36-HttpClientFactoryDemo/HttpClientFactoryDemo/Controllers/OrderController.cs
@@ -38,28 +38,28 @@ namespace HttpClientFactoryDemo.Controllers
 
     //服务
 
-    #region NamedOrderServiceClient.cs
-    public class NamedOrderServiceClient
+#region NamedOrderServiceClient.cs
+public class NamedOrderServiceClient
+{
+    IHttpClientFactory _httpClientFactory;
+
+    const string _clientName = "NamedOrderServiceClient";  //定义客户端名称
+
+    public NamedOrderServiceClient(IHttpClientFactory httpClientFactory)
     {
-        IHttpClientFactory _httpClientFactory;
-
-        const string _clientName = "NamedOrderServiceClient";  //定义客户端名称
-
-        public NamedOrderServiceClient(IHttpClientFactory httpClientFactory)
-        {
-            _httpClientFactory = httpClientFactory;
-        }
-
-
-        public async Task<string> Get()
-        {
-            var client = _httpClientFactory.CreateClient(_clientName); //使用客户端名称获取客户端
-
-            //使用client发起HTTP请求,这里使用相对路径来访问
-            return await client.GetStringAsync("/OrderService");
-        }
+        _httpClientFactory = httpClientFactory;
     }
-    #endregion
+
+
+    public async Task<string> Get()
+    {
+        var client = _httpClientFactory.CreateClient(_clientName); //使用客户端名称获取客户端
+
+        //使用client发起HTTP请求,这里使用相对路径来访问
+        return await client.GetStringAsync("/OrderService");
+    }
+}
+#endregion
 
     #region OrderServiceClient.cs
     public class OrderServiceClient
